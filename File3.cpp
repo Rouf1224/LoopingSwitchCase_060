a586f6ace2b547cb6c507610acbef82f91078ef1
Rouf1224 <ahmadroufr24@gmail.com>
2025-03-18 17:46:31 +0700

    Menambahkan sebuah program luas Persegi Panjang dan Luas lingkaran

--- File3.cpp
+++ File3.cpp
@@ -41,7 +41,13 @@ int main(){
             LuasLingkaran(jejari) << endl;
             cout << endl;
             break;
-    }
-}
+             case 3 :
+            break;
+            default :
+            cout << "Pilihan anda salah!" << endl;
+            cout << endl;
+            break;
+      }
+   }while(pilihan != 3);
 }
 
