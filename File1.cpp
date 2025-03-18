8d0f15a03e604519a5395c5e12681722beb10d8a
Rouf1224 <ahmadroufr24@gmail.com>
2025-03-18 15:39:10 +0700

    Menambahkan sebuah Program Perulangan for

--- File1.cpp
+++ File1.cpp
@@ -16,6 +16,10 @@ int main(){
     //Perulangan for untuk pencacah turun
     cout << "Perulangan for pencacah turun" << endl;
     for (i = 4; i >= 0; i--){
-        cout << "Nilai i : " << i;
+        cout << "Nilai i : " << i
+        << "Lari keliling lapngan" << endl;
+
     } 
+
+    cout << "Nilai i terakhir : " << i << endl;
 }
