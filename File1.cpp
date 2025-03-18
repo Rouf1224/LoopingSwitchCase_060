e53431445e3e0b5587a2e9266d0e5ea1ca3baf9c
Rouf1224 <ahmadroufr24@gmail.com>
2025-03-18 15:25:40 +0700

    Membuat sebuah Program perulangan for

---
+++ File1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Perulangan for intuk pencacahan naik
+    int i;
+    cout << "Perulangan For pencacah Naik" << endl;
+    for (i = 0; i <= 4; i++){
+        cout << "Nilai i : " << i
+        << "Lari Keliling Lapangan" << endl;
+    }
+
+    cout << "Nilai i terakhir : " << i << endl;
+    cout << endl;
+
+    //Perulangan for untuk pencacah turun
+    cout << "Perulangan for pencacah turun" << endl;
+    for (i = 4; i >= 0; i--){
+        cout << "Nilai i : " << i;
+    } 
+}
