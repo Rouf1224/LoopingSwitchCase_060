41d0f636d35c082545a246dcaf5b0df86dc6099f
Rouf1224 <ahmadroufr24@gmail.com>
2025-03-18 16:33:17 +0700

    Membuat sebuah program luas persegipamjamg lingkaran

---
+++ File3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+float LuasPersegiPanjang(float p, float i){
+    return p * i;
+}
+
+float LuasLingkaran(float r){
+    return 3.14 * r * r;
+}
+
+int main(){
+    int pilihan;
+    float panjang, lebar, jejari;
+
+    do{
+        cout << "=====Menu=====" << endl;
+        cout << "1. Menghitung Luas Persegi Panjang" << endl;
+        cout << "2. Menghitung Luas Lingkaran" << endl;
+        cout << "3. Exit" << endl;
+        cout << "Pilihan (1/2/3) :  ";
+        cin >> pilihan;
+
+        switch(pilihan) {
+            
+        }
+    }
+}
