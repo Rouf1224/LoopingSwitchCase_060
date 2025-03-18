868285f768d905a5b06fb483d35c1d15c4e1a5f6
Rouf1224 <ahmadroufr24@gmail.com>
2025-03-18 16:21:26 +0700

    Menambahkan sebuah program perulangan WhiledoWhile

--- File2.cpp
+++ File2.cpp
@@ -18,5 +18,18 @@ int main(){
         x = rand() % 10;
         PerulanganWhile++;
     }
+    cout << "Nilai x akhir : " << x << endl;
+    cout << "Jumlah perulangan :" << PerulanganWhile << endl;
+    cout << endl;
+
+    //Perulangan Do While
+    cout << "Perulanga Do While" << endl;
+    do{
+        cout << "NIlai x : " << x << "Lari keliling Lapangan" << endl;
+        x = rand() % 10;
+        PerulanganWhile++;
+    }while (x < 7);
+    cout << "Nilai x akhir : " << x << endl;
+    cout << "Jumlah Perulangan : " << PerulanganDoWhile << endl;
 }
 
